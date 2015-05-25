06b646922d970a712be8586d8069e1af16942989
austinckai <austinckai1955st@gmail.com>
2015-05-25 10:54:54 +0800

    version xx

--- paper.py
+++ paper.py
@@ -6,10 +6,10 @@ class Delete(webapp2.RequestHandler):
 		data=Data(key_name='paper') //寫入paper
 		data.put()
 	
-		the_data=db.get_by_key_name('paper') //刪除
+		the_data=db.get_by_key_name('papers') //刪除
 		db.delete(the_data)//刪除資料庫
 	
 		query = Paper.all() //刪除多筆資料
 		query.filter('name =','paper')
-		papers = query.fetch(10,10) //刪除10筆資料
+		papers = query.fetch(20,20) //刪除資料
 		db.delete(papers)
